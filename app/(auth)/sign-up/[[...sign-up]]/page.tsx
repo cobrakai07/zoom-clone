fc30bc17726e35636eaa92321794403542e72c1e
Kai <dk786161616@gmail.com>
2024-04-07 17:32:22 +0530

    added clerk auth

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignUp } from "@clerk/nextjs"
+
+const page = () => {
+  return (
+    <main className="flex h-screen w-full items-center justify-center">
+        <SignUp/>
+    </main>
+  )
+}
+
+export default page
